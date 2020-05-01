963adda25a98842881c3eca8b4861c1b664694cc
Veiasai <o206@qq.com>
2020-05-01 11:00:09 +0800

    fix benchmark docker-compose client1 name

--- benchmark/start.sh
+++ benchmark/start.sh
@@ -1,9 +1,15 @@
 # !/bin/bash
 
+docker-compose down
 docker-compose up -d
+
+echo 'wait 5s before testing'
+sleep 5
+
+echo 'apply linux tc'
 
 docker-compose exec client1 tc qdisc add dev eth0 root netem delay 30ms
 docker-compose exec client2 tc qdisc add dev eth0 root netem delay 30ms
 docker-compose exec server tc qdisc add dev eth0 root netem delay 30ms
 
-docker run --rm -i 
+docker-compose exec benchmark /go/src/app/build/tester random
