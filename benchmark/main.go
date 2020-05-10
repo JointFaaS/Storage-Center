4b7bac659a42561b94bfa01f14974781f3e84db8
Veiasai <o206@qq.com>
2020-05-10 14:35:56 +0800

    benchmark: tc and result.txt

--- benchmark/main.go
+++ benchmark/main.go
@@ -13,52 +13,59 @@ import (
 )
 
 var (
-	clientAddr *[]string = &[]string{}
-	clients *[]*utils.Client = &[]*utils.Client{}
+	clientAddr *[]string        = &[]string{}
+	clients    *[]*utils.Client = &[]*utils.Client{}
 )
 
 var rootCmd = &cobra.Command{
 	Use:   "sc-tester",
 	Short: "simple tester",
 	PreRun: func(cmd *cobra.Command, args []string) {
-		
+
 	},
 }
 
 var randomCmd = &cobra.Command{
-	Use: "random",
+	Use:   "random",
 	Short: "random test",
 	Run: func(cmd *cobra.Command, args []string) {
 		var m map[string]string = make(map[string]string)
 		var count int = 100
 		var length int = len(*clients)
-		var timeSlice []time.Duration = make([]time.Duration, 100, 100)
+		var timeSlice []time.Duration = make([]time.Duration, 1000, 1000)
+		var indexRecord []int = make([]int, 1000, 1000)
 		start := time.Now()
 		for i := 0; i < count; i++ {
 			key := strconv.FormatInt(rand.Int63(), 10)
 			index := rand.Int() % length
 			m[key] = strconv.FormatInt(rand.Int63(), 10)
-			(*clients)[index].Set(key, m[key])
-
-			for j := 0; j < 3; j++ {
+			err := (*clients)[index].Set(key, m[key])
+			if err != nil {
+				panic(err)
+			}
+			indexRecord[i*10] = index
+			timeSlice[i*10] = time.Since(start)
+			for j := 1; j < 10; j++ {
 				index = rand.Int() % length
+				indexRecord[i*10+j] = index
 				s, err := (*clients)[index].Get(key)
 				if err != nil {
 					panic(err)
-				} else if (*s != m[key]) {
+				} else if *s != m[key] {
 					log.Printf("Unexpected Value %s, the record is %s\n", *s, m[key])
 				}
+				timeSlice[i*10+j] = time.Since(start)
 			}
-			timeSlice[i] = time.Since(start)
 		}
-		for _, t := range timeSlice {
-			log.Println(t)
+		for i := 0; i < 1000; i++ {
+			log.Println(int(timeSlice[i]))
+			log.Println(indexRecord[i])
 		}
 	},
 }
 
 var localityCmd = &cobra.Command{
-	Use: "locality",
+	Use:   "locality",
 	Short: "locality test",
 	Run: func(cmd *cobra.Command, args []string) {
 
@@ -66,7 +73,7 @@ var localityCmd = &cobra.Command{
 }
 
 var worstCmd = &cobra.Command{
-	Use: "worst",
+	Use:   "worst",
 	Short: "worst test",
 	Run: func(cmd *cobra.Command, args []string) {
 
--- benchmark/start.sh
+++ benchmark/start.sh
@@ -10,10 +10,23 @@ docker-compose up -d
 
 echo 'apply linux tc'
 
-docker-compose exec client1 tc qdisc add dev eth0 root netem delay 30ms
-docker-compose exec client2 tc qdisc add dev eth0 root netem delay 30ms
+# export client1=`docker-compose exec client1 tail -n 1 /etc/hosts | sed 's/\t/\n/g' | head -n 1`
+# export client2=`docker-compose exec client2 tail -n 1 /etc/hosts | sed 's/\t/\n/g' | head -n 1`
+# export server=`docker-compose exec server tail -n 1 /etc/hosts | sed 's/\t/\n/g' | head -n 1`
+
+# docker-compose exec client1 tc qdisc add dev eth0 root handle 1: prio bands 4
+# docker-compose exec client1 tc qdisc add dev eth0 parent 1:4 handle 40: netem delay 30ms
+# docker-compose exec client1 tc filter add dev eth0 protocol ip parent 1:0 prio 4 u32 match ip dst $client2 flowid 1:4
+
+# docker-compose exec client2 tc qdisc add dev eth0 root handle 1: prio bands 4
+# docker-compose exec client2 tc qdisc add dev eth0 parent 1:4 handle 40: netem delay 30ms
+# docker-compose exec client2 tc filter add dev eth0 protocol ip parent 1:0 prio 4 u32 match ip dst $client1 flowid 1:4
+
 docker-compose exec server tc qdisc add dev eth0 root netem delay 30ms
+
+echo 'wait 5s before client ready'
+sleep 5
 
 echo 'random test'
 
-docker-compose exec benchmark /go/src/app/build/tester random
+docker-compose exec benchmark /go/src/app/build/tester random >> result.txt
